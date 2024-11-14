40e2644eab6cb0c72baf8ebb93b2ad9b06c7fb0f
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-14 14:22:51 -0500

    Refactor grid creation and add button to set size
    Now the creation of the grid is divided into generation, deletion and
    creation to help distinguish between the different stages.

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".user-prompt");
+let grid = document.querySelectorAll(".container > div");
 
-for (let i = 0; i < 16; i++) {
-  let grid = document.createElement("div");
-  container.appendChild(grid);
+function generateGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      let newDiv = document.createElement("div");
+      newDiv.style.height = `${750 / gridSize}px`;
+      newDiv.style.width = `${750 / gridSize}px`;
+      container.appendChild(newDiv);
+    }
+  }
+
+  grid = document.querySelectorAll(".container > div");
+  grid.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+    square.addEventListener("mouseout", () => {
+      square.style.backgroundColor = "white";
+    });
+  });
 }
 
-const grid = document.querySelectorAll(".container > div");
-grid.forEach((element) => {
-  element.addEventListener("mouseover", () => {
-    element.style.backgroundColor = "black";
-  });
-  element.addEventListener("mouseout", () => {
-    element.style.backgroundColor = "white";
-    console.log("test");
-  });
-});
+function deleteGrid() {
+  grid = document.querySelectorAll(".container > div");
+  while (container.hasChildNodes()) {
+    grid.forEach((element) => {
+      container.removeChild(element);
+    });
+  }
+}
+
+function createGrid() {
+  let gridSize = prompt("Enter the size of the Grid (< 100): ");
+  while (gridSize > 100) {
+    alert("The number is too big, select a number less than or equal to 100");
+    gridSize = prompt("Enter the size of the Grid (< 100): ");
+  }
+  deleteGrid();
+  generateGrid(gridSize);
+}
+
+button.addEventListener("click", createGrid);
