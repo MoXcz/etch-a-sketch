05b2f4d630abad8498399e3f408cf0c0240c4288
debian-mocos <130593875+MoXcz@users.noreply.github.com>
2024-11-14 13:40:16 -0500

    Add hover effect in div elements
    Two event listeners are added for both moving over and out the divs.
    This creates simple interactivity for coloring.

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ for (let i = 0; i < 16; i++) {
   let grid = document.createElement("div");
   container.appendChild(grid);
 }
+
+const grid = document.querySelectorAll(".container > div");
+grid.forEach((element) => {
+  element.addEventListener("mouseover", () => {
+    element.style.backgroundColor = "black";
+  });
+  element.addEventListener("mouseout", () => {
+    element.style.backgroundColor = "white";
+    console.log("test");
+  });
+});
